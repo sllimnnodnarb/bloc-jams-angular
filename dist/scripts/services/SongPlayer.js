a57537ac01b2bc4e665b246eaf8f287ca6375cc2
Brandon Mills <sllimnodnarb@gmail.com>
2017-01-26 14:52:54 -0600

    adding all files associated with assignment 7

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,15 +2,23 @@
      function SongPlayer() {
          var SongPlayer = {};
          
+/**
+ * @desc the current playing song
+ * @type {Object}
+ */
          var currentSong = null;
-         /**
+/**
  * @desc Buzz object audio file
  * @type {Object}
  */
-
          var currentBuzzObject = null;
          
-         /**
+         var playSong = function() {
+             currentBuzzObject.play();
+             song.playing = true
+         };
+         
+/**
  * @function setSong
  * @desc Stops currently playing song and loads new audio file as currentBuzzObject
  * @param {Object} song
@@ -29,19 +37,27 @@
             currentSong = song;
         };
          
+/**
+ * @function SongPlayer.play
+ * @desc if currentSong is not the song, it makes it so, then plays the song, and sets playing to true.  otherwise, if the song is the song, then the song is in the paused state, and gets played
+ * @param {object} song
+ */       
          SongPlayer.play = function(song) {
                 if (currentSong !== song) {
-                setSong(song);    
-                currentBuzzObject.play();
-                song.playing = true;
+                    setSong(song);    
+                    playSong();
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
                 }
             }
 
-         };
-         
+         };        
+ /**
+ * @function SongPlayer.pause
+ * @desc pauses the current song and sets playing value to false
+ * @param {object} song
+ */
          SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;
